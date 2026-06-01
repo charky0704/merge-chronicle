bdb988f1f707e2f0fb1e808253ad9028aa5941a7
チャーキー <chiamiya@outlook.com>
2026-06-01 21:33:13 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
+let enemyHp = 100;
 
+function attack() {
+  const damage = Math.floor(Math.random() * 20) + 5;
+  enemyHp -= damage;
+
+  document.getElementById("enemyHp").textContent = enemyHp;
+
+  if (enemyHp <= 0) {
+    alert("勝利！");
+    enemyHp = 100;
+    document.getElementById("enemyHp").textContent = enemyHp;
+  }
+}
